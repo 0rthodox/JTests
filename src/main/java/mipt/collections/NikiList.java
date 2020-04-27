b0318269b0f566eded08de89363b25643d8d6a86
0rthodox <long.popov@gmail.com>
2020-04-27 22:28:53 +0300

    tests*

---
+++ src/main/java/mipt/collections/NikiList.java
@@ -0,0 +1,130 @@
+package mipt.collections;
+
+public class NikiList implements CustomList {
+
+    private static final int INITIAL_CAPACITY = 7;
+    private static final int TIMES_OF_ENLARGEMENT = 2;
+    private int size;
+    private int capacity;
+    private Object[] data;
+
+    public NikiList(int capacity) {
+        if (capacity <= 0) {
+            this.capacity = 1;
+        } else {
+            this.capacity = capacity;
+        }
+        data = new Object[this.capacity];
+        size = 0;
+    }
+
+    public NikiList() {
+        this(INITIAL_CAPACITY);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object element) {
+        if (element != null && !isEmpty()) {
+            for (Object currentElement : data) {
+                if (currentElement.equals(element)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(Object element) {
+        if (element == null) {
+            return false;
+            //throw new java.lang.IllegalArgumentException("Please do not add null to me");
+        }
+        if (size == capacity) {
+            enlarge();
+        }
+        data[size++] = element;
+        return true;
+    }
+
+    private void enlarge() {
+        capacity = TIMES_OF_ENLARGEMENT * capacity;
+        Object[] newData = new Object[capacity];
+        System.arraycopy(data, 0, newData, 0, size);
+        data = newData;
+    }
+
+    @Override
+    public boolean remove(Object element) {
+        boolean result = false;
+        int foundIndex = find(element);
+        while (foundIndex != size) {
+            if (foundIndex + 1 != size) {
+                System.arraycopy(data, foundIndex + 1, data, foundIndex, size - foundIndex - 1);
+            }
+            size--;
+            result = true;
+            foundIndex = find(element);
+        }
+        return result;
+    }
+
+    public int find(Object element) {
+        for (int i = 0; i < size; ++i) {
+            if (element.equals(data[i])) {
+                return i;
+            }
+        }
+        return size;
+    }
+
+    @Override
+    public boolean containsAll(CustomList anotherCustomList) {
+        return (anotherCustomList.size() == size && containsSublist(anotherCustomList));
+    }
+
+    @Override
+    public boolean containsSublist(CustomList anotherCustomList) {
+        if (anotherCustomList.isEmpty()) {
+            return true;
+        }
+        for (int i = 0; i < size; ++i) {
+            if (data[i].equals(anotherCustomList.get(0))) { // checking first element
+                if (checkEquity(anotherCustomList, i)) { // checking next size(sublist) elements
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkEquity(CustomList anotherCustomList, int foundFirstsIndex) {
+        if (size - foundFirstsIndex < anotherCustomList.size()) {
+            return false;
+        }
+        for (int i = 0; i < anotherCustomList.size(); ++i) {
+            if (!data[i + foundFirstsIndex].equals(anotherCustomList.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            return new IndexOutOfBoundsException("Wrong index");
+        }
+        return data[index];
+    }
+}
---
+++ src/test/java/mipt/collections/NikiListTest.java
@@ -0,0 +1,111 @@
+package mipt.collections;
+
+import static org.junit.Assert.*;
+
+public class NikiListTest {
+
+    @org.junit.Test
+    public void size() {
+        CustomList list = new NikiList();
+        assertEquals(0, list.size());
+        list.add(3);
+        list.add(42);
+        assertEquals(2, list.size());
+    }
+
+    @org.junit.Test
+    public void isEmpty() {
+        CustomList list = new NikiList();
+        assertTrue(list.isEmpty());
+        list.add(42);
+        assertFalse(list.isEmpty());
+        list.remove(42);
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void contains() {
+        CustomList list = new NikiList();
+        assertFalse(list.contains(42));
+        list.add(42);
+        assertTrue(list.contains(42));
+    }
+
+    @org.junit.Test
+    public void add() {
+        CustomList list = new NikiList();
+        list.add(42);
+        assertTrue(list.contains(42));
+        list.add(0x42);
+        list.add(042);
+        assertTrue(list.contains(0x42));
+        assertEquals(3, list.size());
+    }
+
+    @org.junit.Test
+    public void remove() {
+        CustomList list = new NikiList();
+        list.add(42);
+        list.remove(42);
+        assertTrue(list.isEmpty());
+    }
+
+    @org.junit.Test
+    public void find() {
+        NikiList list = new NikiList();
+        list.add(42);
+        list.find(42);
+        assertEquals(0, list.find(42));
+        list.add(42);
+        assertEquals(0, list.find(42));
+        assertEquals(list.size(), list.find(042));
+    }
+
+    @org.junit.Test
+    public void containsAll() {
+        CustomList list = new NikiList();
+        list.add(42);
+        list.add(042);
+        list.add(0x42);
+        CustomList anotherList = new NikiList();
+        anotherList.add(42);
+        anotherList.add(042);
+        anotherList.add(0x42);
+        assertTrue(list.containsAll(anotherList));
+        anotherList.remove(042);
+        assertFalse(list.containsAll(anotherList));
+        list.remove(042);
+        assertTrue(list.containsAll(anotherList));
+        list.remove(0x42);
+        assertFalse(list.containsAll(anotherList));
+    }
+
+    @org.junit.Test
+    public void containsSublist() {
+        CustomList list = new NikiList();
+        list.add(42);
+        list.add(042);
+        list.add(0x42);
+        CustomList anotherList = new NikiList();
+        anotherList.add(42);
+        anotherList.add(042);
+        assertTrue(list.containsSublist(anotherList));
+        list.remove(0x42);
+        assertTrue(list.containsAll(anotherList));
+        list.remove(042);
+        assertFalse(list.containsAll(anotherList));
+    }
+
+    @org.junit.Test
+    public void get() {
+        CustomList list = new NikiList();
+        try {
+            list.get(1);
+        } catch (Throwable ex) {
+            assertTrue(ex instanceof IndexOutOfBoundsException);
+            assertEquals("Wrong index", ex.getMessage());
+        }
+        list.add(42);
+        assertEquals(42, list.get(0));
+    }
+}
